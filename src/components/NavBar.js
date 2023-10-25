6640f9a30508a24493c7b9cec5e592fc1e6e70aa
Guyo <guyo.halake@students.moringaschool.com>
2023-10-26 00:01:58 +0300

    completed lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React from 'react';
+import './NavBar.css'; 
 
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <ul className="navbar-list">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+      </ul>
     </nav>
   );
 }
+
 export default NavBar;
